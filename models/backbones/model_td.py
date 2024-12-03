2fb10ce45320a643bf3a07de6178c71bfab32d2c
Ahmed Ghorbel <ahmed.ghorbel888@gmail.com>
2024-12-03 20:47:45 +0100

    implement NexTune bakcbone

--- models/backbones/model_td.py
+++ models/backbones/model_td.py
@@ -1,7 +1,6 @@
 import math
 import torch
 import torch.nn as nn
-import torchvision.models as models
 from models.backbones.layers import modulate, AdaTransformer
 
 
@@ -49,38 +48,13 @@ class TimestepEmbedder(nn.Module):
         t_emb = self.mlp(t_freq)
         return t_emb
 
-class MapEmbedder(nn.Module):
-    """
-    Map encoding using EfficientNet-B0 as context to condition the TrafficDiffuser.
-    """ 
-    def __init__(self, map_channels, hidden_size):
-        super().__init__()      
-        # Load pretrained EfficientNet-B0
-        self.efficientnet = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
-        # Modify the first convolution layer to accept 4-channel input
-        self.efficientnet.features[0][0] = nn.Conv2d(map_channels, 32, kernel_size=3, stride=2, padding=1, bias=False)
-        # Remove the final fully connected layer
-        self.efficientnet = nn.Sequential(*list(self.efficientnet.children())[:-2])
-        
-        # Final projection and normalization
-        self.norm_final = nn.LayerNorm(1280, elementwise_affine=False, eps=1e-6)
-        self.proj_final = nn.Linear(1280, hidden_size, bias=True)
-
-    def forward(self, x):
-        # (B, C, H, W)
-        x = self.efficientnet(x)        # (B, 1280, 7, 7)
-        x = x.mean(dim=[2, 3])          # Global average pooling (B, 1280)
-        x = self.norm_final(x)          # (B, 1280)
-        x = self.proj_final(x)          # (B, hidden_size)
-        return x
-
 
 #################################################################################
-#                       Core TrafficDiffuser Model                              #
+#                       Core NexTune Model                              #
 #################################################################################
 class FinalLayer(nn.Module):
     """
-    The final layer of the TrafficDiffuser
+    The final layer of the NexTune
     Used for adaLN, project x the to desired output size.
     """
     def __init__(self, max_num_agents, seq_length, dim_size, hidden_size):
@@ -103,7 +77,7 @@ class FinalLayer(nn.Module):
         x = x.reshape(-1, self.max_num_agents, self.seq_length, self.dim_size)  # (B, N, L, D)
         return x
     
-class TrafficDiffuser(nn.Module):
+class NexTune(nn.Module):
     """
     Diffusion backbone with Transformer layers.
     """
@@ -113,8 +87,6 @@ class TrafficDiffuser(nn.Module):
         seq_length,
         hist_length,
         dim_size,
-        map_channels,
-        use_map_embed,
         use_ckpt_wrapper,
         hidden_size,
         num_heads,
@@ -122,13 +94,7 @@ class TrafficDiffuser(nn.Module):
         mlp_ratio=4.0,
     ):
         super().__init__()
-        self.t_embedder = TimestepEmbedder(hidden_size)
-        if use_map_embed:
-            self.m_embedder = MapEmbedder(
-                map_channels=map_channels,
-                hidden_size=hidden_size,
-            )
-            
+        self.t_embedder = TimestepEmbedder(hidden_size)         
         self.proj1 = nn.Linear(dim_size, hidden_size, bias=True)
         
         self.t_pos_embed = nn.Parameter(torch.zeros(1, hist_length+seq_length, hidden_size), requires_grad=True)
@@ -136,10 +102,8 @@ class TrafficDiffuser(nn.Module):
             AdaTransformer(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)
         ])
         
-        self.final_layer = FinalLayer(max_num_agents, hist_length+seq_length, dim_size, hidden_size) 
-        
+        self.final_layer = FinalLayer(max_num_agents, hist_length+seq_length, dim_size, hidden_size)    
         self.hist_length = hist_length
-        self.use_map_embed = use_map_embed
         self.use_ckpt_wrapper = use_ckpt_wrapper
         self.initialize_weights()
 
@@ -173,67 +137,61 @@ class TrafficDiffuser(nn.Module):
             return outputs
         return ckpt_forward
     
-    def forward(self, x, t, h, m=None):
+    def forward(self, x, t, h):
         """
-        Forward pass of TrafficDiffuser.
-        - x: (B, N, L_x, D) tensor of agents where N:max_num_agents, L_x:sequence_length, and D:dim representing (x, y) positions
+        Forward pass of NexTune.
+        - x: (B, N, L_x) tensor of future audio chunk where N:n_mels, L_x:sequence_length
         - t: (B,) tensor of diffusion timesteps     
-        - h: (B, N, L_h, D) tensor of history agents where N:max_num_agents, L_h:hist_sequence_length, and D:dim representing (x, y) positions
-        - m: (B, H, W, C) tensor of the rasterized map
+        - h: (B, N, L_h) tensor of history agents where N:max_num_agents, L_h:hist_sequence_length
         """
         
         ##################### Cat and Proj ##########################
-        # (B, N, L_x, D), (B, N, L_h, D)
-        x = torch.cat((h, x), dim=2)                    # (B, N, L, D)
-        x = self.proj1(x)                               # (B, N, L, H)
-        B, N, L, H = x.shape[0], x.shape[1], x.shape[2], x.shape[3]
+        # (B, N, L_x), (B, N, L_h)
+        x = torch.cat((h, x), dim=2)                    # (B, N, L)
+        x = x.permute(0, 2, 1)                          # (B, L, N)
+        x = self.proj1(x)                               # (B, L, H)
+        B, L, H = x.shape[0], x.shape[1], x.shape[2]
         #############################################################
         
         ###################### Embedders ############################
-        # (B, t_max=1000), (B, H, W, C)
-        c = self.t_embedder(t)                          # (B, H) ts for diff process
-        if self.use_map_embed:
-            c = c + self.m_embedder(m)                  # (B, H)
-        c = c.unsqueeze(1)                              # (B, 1, H)
-        c = c.expand(B, N, H).contiguous()              # (B, N, H)
-        c = c.reshape(B*N, H)                           # (B*N, H)
+        # (B, t_max=1000)
+        c = self.t_embedder(t)                          # (B, H)
         #############################################################
         
         ################# Temporal Attention ########################
-        # (B, N, L, H), (B*N, H)
-        x = x.reshape(B*N, L, H)                        # (B*N, L, H)
-        x = x + self.t_pos_embed                        # (B*N, L, H)
+        # (B, L, H), (B, H)
+        x = x + self.t_pos_embed                        # (B, L, H)
         if self.use_ckpt_wrapper:
             for block in self.t_blocks:
                 x = torch.utils.checkpoint.checkpoint(self.ckpt_wrapper(block), x, c, use_reentrant=False)
         else:
             for block in self.t_blocks:
-                x = block(x, c)                         # (B*N, L, H)
+                x = block(x, c)                         # (B, L, H)
         #############################################################
         
         ##################### Final layer ###########################
-        # (B*N, L, H), (B*N, H)
-        x = self.final_layer(x, c)                      # (B, N, L, D)
+        # (B, L, H), (B, H)
+        x = self.final_layer(x, c)                      # (B, N, L)
         #############################################################
         
-        return x[:, :, self.hist_length:, :]
+        return x[:, :, self.hist_length:]
 
 
 #################################################################################
-#                          TrafficDiffuser Configs                              #
+#                          NexTune Configs                              #
 #################################################################################
 
-def TrafficDiffuser_L(**kwargs):
-    return TrafficDiffuser(hidden_size=512, num_heads=16, depth=24, **kwargs)
+def NexTune_L(**kwargs):
+    return NexTune(hidden_size=1024, num_heads=16, depth=24, **kwargs)
 
-def TrafficDiffuser_B(**kwargs):
-    return TrafficDiffuser(hidden_size=384, num_heads=12, depth=22, **kwargs)
+def NexTune_B(**kwargs):
+    return NexTune(hidden_size=768, num_heads=12, depth=22, **kwargs)
 
-def TrafficDiffuser_S(**kwargs):
-    return TrafficDiffuser(hidden_size=128, num_heads=8, depth=16, **kwargs)
+def NexTune_S(**kwargs):
+    return NexTune(hidden_size=512, num_heads=8, depth=16, **kwargs)
 
-TrafficDiffuser_models = {
-    'TrafficDiffuser-L': TrafficDiffuser_L,
-    'TrafficDiffuser-B': TrafficDiffuser_B,
-    'TrafficDiffuser-S': TrafficDiffuser_S,
+NexTune_models = {
+    'NexTune-L': NexTune_L,
+    'NexTune-B': NexTune_B,
+    'NexTune-S': NexTune_S,
 }
